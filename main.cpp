9ad7fcc163e8b819f420317489e3ac0eff8f025c
Alvineornela <127629007+Alvineornela@users.noreply.github.com>
2023-05-21 22:23:04 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+#include "lib.h"
 int main(){
+ char i;
+    cin>>i;
+    bool a;
+    a= conversione(&i);
+    if (a){
+        cout<<i<<endl;
+    }
+    else{
+        cout<<"errore"<<endl;
+    } 
+  
+  
   return 0;
 }
