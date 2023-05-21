45712280e07ee3cc5491832aa26cc1788928147e
Alvineornela <127629007+Alvineornela@users.noreply.github.com>
2023-05-21 22:21:08 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
 
+#include "lib.h"
+bool conversione (char* i){
+    if((*i>='a')&&(*i<='z')){
+        *i=(*i)-32;
+        return true;
+    }else if((*i>='A')&&(*i<='Z')){
+        *i=(*i)+32;
+        return true;
+    }else{
+        return false;
+    }
+
+}
+
